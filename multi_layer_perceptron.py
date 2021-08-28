851e48eb3c836dd7550fd9bae2464c34b3dc2b16
Hasan Ilyas <hasanilyas@outlook.com>
2021-08-28 14:33:17 +0500

    multi layer perceptron program

--- multi_layer_perceptron.py
+++ multi_layer_perceptron.py
@@ -1,78 +1,75 @@
 import numpy as np
-import math
 
 
-def changeWeights(previousWeights, bias, errors, outputs, nodeOutputs, rate, noOfNodes):
-    errors = errors[::-1]
-    newWeights = []
-    newBias = []
-    for i in range(noOfNodes - 1, -1, -1):
-        newWeights.append([
-            previousWeights[i][0] + rate * errors[i] * nodeOutputs[i],
-            previousWeights[i][1] + rate * errors[i] * nodeOutputs[i]
-        ])
-        newBias.append(bias[i] + rate * errors[i])
-    return [np.array(newWeights), np.array(newBias)]
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
 
+def sigmoid_derivative(x):
+    return x * (1 - x)
+
+
+# Input datasets
 inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-outputs = [0, 1, 1, 0]
-rate = 0.8
-noOfNodes = 3
-weights = np.array([[0.2, 0.4], [-0.3, 0.1], [-0.3, -0.2]])
-bias = np.array([-0.4, 0.2, 0.1])
-threshold = 1
-epoch = 1000
+expected_output = np.array([[0], [1], [1], [0]])
 
-print('initial weights', weights)
-print('initial bias', bias)
+epochs = 20000
+lr = 0.8
+inputLayerNeurons, hiddenLayerNeurons, outputLayerNeurons = 2, 2, 1
 
-for itr in range(0, epoch):
-    print('START OF Epoch #', itr)
-    print('#######################################')
-    for input in inputs:
-        lastNodeInputs = []
-        nodeOutputs = []
-        netInputs = []
-        lastNodeError = 0
-        lastNodeIndex = 0
-        errors = []
-        sumOfErrors = 0
-        for i in range(0, noOfNodes):
-            if (i < 2):
-                product = weights[i] * input
-            else:
-                product = weights[i] * np.array(lastNodeInputs)
-                lastNodeIndex = i
-            netInput = np.sum(product) + bias[i]
-            netInputs.append(netInput)
-            output = 1 / (1 + math.exp(-1 * netInput))
-            lastNodeInputs.append(output)
-            nodeOutputs.append(output)
+hidden_weights = np.array([[0.2, 0.4], [-0.3, 0.1]])
+hidden_bias = np.array([[-0.4, 0.2]])
+output_weights = np.array([[-0.3], [-0.2]])
+output_bias = np.array([[0.1]])
 
-        for i in range(noOfNodes - 1, -1, -1):
-            if (i == 2):
-                lastNodeError = nodeOutputs[i] * (1 - nodeOutputs[i]) * (threshold - nodeOutputs[i])
-                errors.append(lastNodeError)
-            else:
-                error = nodeOutputs[i] * (1 - nodeOutputs[i]) * lastNodeError * weights[lastNodeIndex][i]
-                errors.append(error)
+print("Initial hidden weights: ", end='')
+print(*hidden_weights)
+print("Initial hidden biases: ", end='')
+print(*hidden_bias)
+print("Initial output weights: ", end='')
+print(*output_weights)
+print("Initial output biases: ", end='')
+print(*output_bias)
+print('##############################################')
+# Training algorithm
+for i in range(epochs):
+    print('Start Epoch # ', i)
 
-        print('net inputs', netInputs)
-        print('outputs', nodeOutputs)
-        print('errors', errors)
-        sumOfErrors = sum(errors)
-        print('sum of errors', sumOfErrors)
+    # Forward Propagation
+    hidden_layer_activation = np.dot(inputs, hidden_weights)
+    hidden_layer_activation += hidden_bias
+    hidden_layer_output = sigmoid(hidden_layer_activation)
+    print('hidden_layer_output',*hidden_layer_output)
 
-        if (errors[0] != 0):
-            result = changeWeights(weights, bias, errors, outputs, nodeOutputs, rate, noOfNodes)
-            weights = result[0]
-            bias = result[1]
-            print('new weights', weights)
-            print('new biases', bias)
+    output_layer_activation = np.dot(hidden_layer_output, output_weights)
+    output_layer_activation += output_bias
+    predicted_output = sigmoid(output_layer_activation)
+    print('predicted_output', *predicted_output)
 
-        if (errors[0] == 0):
-            break
+    # Backpropagation
+    error = expected_output - predicted_output
+    d_predicted_output = error * sigmoid_derivative(predicted_output)
 
-    print('END OF Epoch #', itr)
-    print('#######################################')
+    error_hidden_layer = d_predicted_output.dot(output_weights.T)
+    d_hidden_layer = error_hidden_layer * sigmoid_derivative(hidden_layer_output)
+
+    # Updating Weights and Biases
+    output_weights += hidden_layer_output.T.dot(d_predicted_output) * lr
+    output_bias += np.sum(d_predicted_output, axis=0, keepdims=True) * lr
+    hidden_weights += inputs.T.dot(d_hidden_layer) * lr
+    hidden_bias += np.sum(d_hidden_layer, axis=0, keepdims=True) * lr
+
+    print('End Epoch # ', i)
+    print('##############################################')
+
+print("Final hidden weights: ", end='')
+print(*hidden_weights)
+print("Final hidden bias: ", end='')
+print(*hidden_bias)
+print("Final output weights: ", end='')
+print(*output_weights)
+print("Final output bias: ", end='')
+print(*output_bias)
+
+print("\nOutput from neural network after 10,000 epochs: ", end='')
+print(*predicted_output)
